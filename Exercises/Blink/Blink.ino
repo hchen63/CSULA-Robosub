53557e23bb2e32d5e8742faa28a67aef3124e769
Krystal Bernal <krystalbc@sbcglobal.net>
2017-10-06 15:58:28 -0700

    Add files via upload

---
+++ Exercises/Blink/Blink.ino
@@ -0,0 +1,20 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+*/
+
+const int LED = 8; //remember the pin number for the LED
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED as an output.
+  pinMode(LED, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
